1f53a28436d4fe58769609abb64aef39cca36a7a
Hiroyuki Adachi <hiroxpepe@gmail.com>
2025-06-28 17:32:39 +0900

    feat: add new features and XML docs to framework

--- Scripts/Players/States/Human_DoFixedUpdate.cs
+++ Scripts/Players/States/Human_DoFixedUpdate.cs
@@ -13,30 +13,84 @@ namespace Germio {
         #region Inner Classes
 
         /// <summary>
-        /// The Class for the FixedUpdate() method.
+        /// Handles the FixedUpdate() method logic.
         /// </summary>
         protected class DoFixedUpdate {
 
             ///////////////////////////////////////////////////////////////////////////////////////
             // Fields [noun, adjectives] 
 
-            bool _idol, _run, _walk, _jump, _abort_jump, _backward, _stop;
+            /// <summary>
+            /// Indicates whether the player is idle.
+            /// </summary>
+            bool _idol;
+
+            /// <summary>
+            /// Indicates whether the player is running.
+            /// </summary>
+            bool _run;
+
+            /// <summary>
+            /// Indicates whether the player is walking.
+            /// </summary>
+            bool _walk;
+
+            /// <summary>
+            /// Indicates whether the player is jumping.
+            /// </summary>
+            bool _jump;
+
+            /// <summary>
+            /// Indicates whether the player is aborting a jump.
+            /// </summary>
+            bool _abort_jump;
+
+            /// <summary>
+            /// Indicates whether the player is moving backward.
+            /// </summary>
+            bool _backward;
+
+            /// <summary>
+            /// Indicates whether the player is stopping.
+            /// </summary>
+            bool _stop;
 
             ///////////////////////////////////////////////////////////////////////////////////////
             // Properties [noun, adjectives] 
 
+            /// <summary>
+            /// Gets whether the player is idle.
+            /// </summary>
             public bool idol { get => _idol; }
 
+            /// <summary>
+            /// Gets whether the player is running.
+            /// </summary>
             public bool run { get => _run; }
 
+            /// <summary>
+            /// Gets whether the player is walking.
+            /// </summary>
             public bool walk { get => _walk; }
 
+            /// <summary>
+            /// Gets whether the player is jumping.
+            /// </summary>
             public bool jump { get => _jump; }
 
+            /// <summary>
+            /// Gets whether the player is aborting a jump.
+            /// </summary>
             public bool abortJump { get => _abort_jump; }
 
+            /// <summary>
+            /// Gets whether the player is moving backward.
+            /// </summary>
             public bool backward { get => _backward; }
 
+            /// <summary>
+            /// Gets whether the player is stopping.
+            /// </summary>
             public bool stop { get => _stop; }
 
             ///////////////////////////////////////////////////////////////////////////////////////
@@ -52,6 +106,10 @@ namespace Germio {
             ///////////////////////////////////////////////////////////////////////////////////////////
             // Public Methods [verb]
 
+            /// <summary>
+            /// Applies the specified FixedUpdate state.
+            /// </summary>
+            /// <param name="type">The FixedUpdate state to apply.</param>
             public void Apply(FixedUpdate type) {
                 switch (type) {
                     case FixedUpdate.Idol: _idol = true; _run = _walk = _backward = _jump = false; break;
@@ -64,6 +122,10 @@ namespace Germio {
                 }
             }
 
+            /// <summary>
+            /// Cancels the specified FixedUpdate state.
+            /// </summary>
+            /// <param name="type">The FixedUpdate state to cancel.</param>
             public void Cancel(FixedUpdate type) {
                 switch (type) {
                     case FixedUpdate.Idol: break;
@@ -80,13 +142,43 @@ namespace Germio {
         #endregion
     }
 
+    /// <summary>
+    /// Specifies the FixedUpdate states.
+    /// </summary>
     public enum FixedUpdate {
+        /// <summary>
+        /// Idle state.
+        /// </summary>
         Idol,
+
+        /// <summary>
+        /// Running state.
+        /// </summary>
         Run,
+
+        /// <summary>
+        /// Walking state.
+        /// </summary>
         Walk,
+
+        /// <summary>
+        /// Backward movement state.
+        /// </summary>
         Backward,
+
+        /// <summary>
+        /// Jumping state.
+        /// </summary>
         Jump,
+
+        /// <summary>
+        /// Aborting jump state.
+        /// </summary>
         AbortJump,
+
+        /// <summary>
+        /// Stopping state.
+        /// </summary>
         Stop
     }
 }
--- Scripts/Systems/SoundSystem.cs
+++ Scripts/Systems/SoundSystem.cs
@@ -6,41 +6,75 @@ using UnityEngine;
 
 namespace Germio {
     /// <summary>
-    /// The sound system
-    /// <author>h.adachi (STUDIO MeowToon)</author>
+    /// Manages sound effects and background music.
     /// </summary>
+    /// <author>h.adachi (STUDIO MeowToon)</author>
     public class SoundSystem : MonoBehaviour {
 #nullable enable
 
         ///////////////////////////////////////////////////////////////////////////////////////////////
         // References [bool => is+adjective, has+past participle, can+verb prototype, triad verb]
 
+        /// <summary>
+        /// Sound effect for item interaction.
+        /// </summary>
         [SerializeField] AudioClip _se_item_clip;
 
+        /// <summary>
+        /// Sound effect for jumping.
+        /// </summary>
         [SerializeField] AudioClip _se_jump_clip;
 
+        /// <summary>
+        /// Sound effect for climbing.
+        /// </summary>
         [SerializeField] AudioClip _se_climb_clip;
 
+        /// <summary>
+        /// Sound effect for walking.
+        /// </summary>
         [SerializeField] AudioClip _se_walk_clip;
 
+        /// <summary>
+        /// Sound effect for running.
+        /// </summary>
         [SerializeField] AudioClip _se_run_clip;
 
+        /// <summary>
+        /// Sound effect for grounding.
+        /// </summary>
         [SerializeField] AudioClip _se_grounded_clip;
 
+        /// <summary>
+        /// Background music for level beat.
+        /// </summary>
         [SerializeField] AudioClip _bgm_beat_level_clip;
 
         ///////////////////////////////////////////////////////////////////////////////////////////////
         // Fields [noun, adjectives] 
 
+        /// <summary>
+        /// Currently playing sound effect.
+        /// </summary>
         SEClip _now_playing_clip_se1;
 
+        /// <summary>
+        /// Audio source for sound effects.
+        /// </summary>
         AudioSource _audio_source_se1;
 
+        /// <summary>
+        /// Audio source for background music.
+        /// </summary>
         AudioSource _audio_source_bgm;
 
         ///////////////////////////////////////////////////////////////////////////////////////////////
         // Public Methods [verb]
 
+        /// <summary>
+        /// Plays the specified sound effect.
+        /// </summary>
+        /// <param name="type">The type of sound effect to play.</param>
         public void Play(SEClip type) {
             switch (type) {
                 case SEClip.Item:
@@ -101,6 +135,10 @@ namespace Germio {
             }
         }
 
+        /// <summary>
+        /// Plays the specified background music.
+        /// </summary>
+        /// <param name="type">The type of background music to play.</param>
         public void Play(BGMClip type) {
             switch (type) {
                 case BGMClip.BeatLevel:
@@ -111,6 +149,9 @@ namespace Germio {
             }
         }
 
+        /// <summary>
+        /// Stops the currently playing sound effect.
+        /// </summary>
         public void StopSEClip() {
             if (_now_playing_clip_se1 != SEClip.Grounded && _now_playing_clip_se1 != SEClip.Item) {
                 _audio_source_se1.Stop();
@@ -127,17 +168,53 @@ namespace Germio {
         }
     }
 
+    /// <summary>
+    /// Specifies the types of sound effects.
+    /// </summary>
     public enum SEClip {
+        /// <summary>
+        /// Sound effect for item interaction.
+        /// </summary>
         Item,
+
+        /// <summary>
+        /// Sound effect for jumping.
+        /// </summary>
         Jump,
+
+        /// <summary>
+        /// Sound effect for climbing.
+        /// </summary>
         Climb,
+
+        /// <summary>
+        /// Sound effect for walking.
+        /// </summary>
         Walk,
+
+        /// <summary>
+        /// Sound effect for running.
+        /// </summary>
         Run,
+
+        /// <summary>
+        /// Sound effect for grounding.
+        /// </summary>
         Grounded,
+
+        /// <summary>
+        /// Sound effect for pushing.
+        /// </summary>
         Push
     }
 
+    /// <summary>
+    /// Specifies the types of background music.
+    /// </summary>
     public enum BGMClip {
+        /// <summary>
+        /// Background music for level beat.
+        /// </summary>
         BeatLevel
     }
 }
